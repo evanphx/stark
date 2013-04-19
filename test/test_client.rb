c7ccb58cf7b410cf7bf4dd5b28aec95cef6773c9
Nick Sieger <nick@nicksieger.com>
2013-04-19 14:00:49 -0500

    Reduce duplication in test_client

--- test/test_client.rb
+++ test/test_client.rb
@@ -88,24 +88,25 @@ class TestClient < Test::Unit::TestCase
     end
   end
 
+  def send_to_server
+    st = Thread.new do
+      @server.process @server_p, @server_p
+    end
+    yield
+  ensure
+    st.join
+  end
+
   def test_store_and_retrieve
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      xuser = @n::UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
+
+      @client.store xuser
     end
 
-    xuser = @n::UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
-
-    @client.store xuser
-
-    st.join
-
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    obj = send_to_server do
+      @client.retrieve 0
     end
-
-    obj = @client.retrieve 0
-
-    st.join
 
     assert_equal 0, obj.uid
     assert_equal "root", obj.name
@@ -113,114 +114,84 @@ class TestClient < Test::Unit::TestCase
   end
 
   def test_set_map
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+
+      m = { "blah" => "foo", "a" => "b" }
+
+      @client.set_map m
+
     end
-
-    m = { "blah" => "foo", "a" => "b" }
-
-    @client.set_map m
-
-    st.join
 
     assert_equal "foo", @handler.last_map["blah"]
     assert_equal "b", @handler.last_map["a"]
   end
 
   def test_last_map
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    m = send_to_server do
+      @handler.last_map = { "blah" => "foo", "a" => "b" }
+
+      @client.last_map
     end
-
-    @handler.last_map = { "blah" => "foo", "a" => "b" }
-
-    m = @client.last_map
-
-    st.join
 
     assert_equal "foo", m["blah"]
     assert_equal "b", m["a"]
   end
 
   def test_set_list
-    st = Thread.new do
-      @server.process @server_p, @server_p
-    end
-
     m = [ "blah", "foo", "a", "b" ]
 
-    @client.set_list m
-
-    st.join
+    send_to_server do
+      @client.set_list m
+    end
 
     assert_equal m, @handler.last_list
   end
 
   def test_last_list
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      l = [ "blah", "foo", "a", "b" ]
+      @handler.last_list = l
+
+      begin
+        assert_equal l, @client.last_list
+      rescue Interrupt => e
+        puts e.backtrace
+      end
     end
-
-    l = [ "blah", "foo", "a", "b" ]
-    @handler.last_list = l
-
-    begin
-      assert_equal l, @client.last_list
-    rescue Interrupt => e
-      puts e.backtrace
-    end
-
-    st.join
   end
 
   def test_last_list_is_nil
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      begin
+        assert_equal nil, @client.last_list
+      rescue Interrupt => e
+        puts e.backtrace
+      end
     end
-
-    begin
-      assert_equal nil, @client.last_list
-    rescue Interrupt => e
-      puts e.backtrace
-    end
-
-    st.join
   end
 
   def test_enum
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      @client.set_status :ON
     end
-
-    @client.set_status :ON
-
-    st.join
 
     assert_equal 0, @handler.last_status
   end
 
   def test_enum_recv
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      @handler.last_status = 0
+
+      assert_equal :ON, @client.last_status
     end
-
-    @handler.last_status = 0
-
-    assert_equal :ON, @client.last_status
-
-    st.join
   end
 
   def test_throw
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    e = send_to_server do
+      assert_raises @n::RockTooHard do
+        @client.volume_up
+      end
     end
-
-    e = assert_raises @n::RockTooHard do
-      @client.volume_up
-    end
-
-    st.join
 
     assert_equal 11, e.volume
   end
@@ -228,142 +199,110 @@ class TestClient < Test::Unit::TestCase
   # Thread.abort_on_exception = true
 
   def test_oneway
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      t = Time.now
+
+      Timeout.timeout 3 do
+        assert_equal nil, @client.make_bitcoins
+      end
+
+      assert Time.now - t < 0.1
     end
-
-    t = Time.now
-
-    Timeout.timeout 3 do
-      assert_equal nil, @client.make_bitcoins
-    end
-
-    assert Time.now - t < 0.1
-
-    st.join
   end
 
   def test_2args
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      assert_equal 7, @client.add(3, 4)
     end
-
-    assert_equal 7, @client.add(3, 4)
-
-    st.join
   end
 
   def test_read_struct_in_a_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      prof = UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
+      stat = UserStatus.new 'profile' => prof, 'active' => true
+
+      @handler.user_status = stat
+
+      status = @client.user_status
+
+      assert_equal true, status.active
+
+      prof = status.profile
+
+      assert_equal 0, prof.uid
+      assert_equal "root", prof.name
+      assert_equal "god", prof.blurb
     end
-
-    prof = UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
-    stat = UserStatus.new 'profile' => prof, 'active' => true
-
-    @handler.user_status = stat
-
-    status = @client.user_status
-
-    assert_equal true, status.active
-    
-    prof = status.profile
-
-    assert_equal 0, prof.uid
-    assert_equal "root", prof.name
-    assert_equal "god", prof.blurb
-
-    st.join
   end
 
   def test_write_struct_in_a_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      prof = @n::UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
+      stat = @n::UserStatus.new 'profile' => prof, 'active' => true
+
+      @client.set_user_status stat
+
+      status = @handler.user_status
+
+      assert_equal true, status.active
+
+      prof = status.profile
+
+      assert_equal 0, prof.uid
+      assert_equal "root", prof.name
+      assert_equal "god", prof.blurb
     end
-
-    prof = @n::UserProfile.new 'uid' => 0, 'name' => 'root', 'blurb' => 'god'
-    stat = @n::UserStatus.new 'profile' => prof, 'active' => true
-
-    @client.set_user_status stat
-
-    status = @handler.user_status
-
-    assert_equal true, status.active
-    
-    prof = status.profile
-
-    assert_equal 0, prof.uid
-    assert_equal "root", prof.name
-    assert_equal "god", prof.blurb
-
-    st.join
   end
 
   def test_read_enum_in_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      stat = UserRelationship.new 'user' => 0, 'status' => 4
+
+      @handler.user_relationship = stat
+
+      rel = @client.user_relationship
+
+      assert_equal 0, rel.user
+      assert_equal :ITS_COMPLICATED, rel.status
     end
-
-    stat = UserRelationship.new 'user' => 0, 'status' => 4
-
-    @handler.user_relationship = stat
-
-    rel = @client.user_relationship
-
-    assert_equal 0, rel.user
-    assert_equal :ITS_COMPLICATED, rel.status
-
-    st.join
   end
 
   def test_write_enum_in_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      stat = @n::UserRelationship.new 'user' => 0, 'status' => :ITS_COMPLICATED
+
+      @client.set_user_relationship stat
+
+      rel = @handler.user_relationship
+
+      assert_equal 0, rel.user
+      assert_equal 4, rel.status
     end
-
-    stat = @n::UserRelationship.new 'user' => 0, 'status' => :ITS_COMPLICATED
-
-    @client.set_user_relationship stat
-
-    rel = @handler.user_relationship
-
-    assert_equal 0, rel.user
-    assert_equal 4, rel.status
-
-    st.join
   end
 
   def test_read_list_in_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      stat = UserFriends.new 'user' => 0, 'friends' => [4,8,47]
+
+      @handler.user_friends = stat
+
+      rel = @client.user_friends
+
+      assert_equal 0, rel.user
+      assert_equal [4,8,47], rel.friends
     end
-
-    stat = UserFriends.new 'user' => 0, 'friends' => [4,8,47]
-
-    @handler.user_friends = stat
-
-    rel = @client.user_friends
-
-    assert_equal 0, rel.user
-    assert_equal [4,8,47], rel.friends
-
-    st.join
   end
 
   def test_write_list_in_struct
-    st = Thread.new do
-      @server.process @server_p, @server_p
+    send_to_server do
+      stat = @n::UserFriends.new 'user' => 0, 'friends' => [4,8,47]
+
+      @client.set_user_friends stat
+
+      rel = @handler.user_friends
+
+      assert_equal 0, rel.user
+      assert_equal [4,8,47], rel.friends
     end
-
-    stat = @n::UserFriends.new 'user' => 0, 'friends' => [4,8,47]
-
-    @client.set_user_friends stat
-
-    rel = @handler.user_friends
-
-    assert_equal 0, rel.user
-    assert_equal [4,8,47], rel.friends
-
-    st.join
   end
 end
