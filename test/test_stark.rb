86f856fcd0484b58fa0504c38434e28bd6e19cd3
Nick Sieger <nick@nicksieger.com>
2013-04-23 11:05:28 -0500

    Change name to test_stark

--- test/test_stark.rb
+++ test/test_stark.rb
@@ -5,8 +5,10 @@ require 'stark'
 require 'rubygems'
 require 'thrift'
 
-class TestSpark < Test::Unit::TestCase
-  module TestSparkScope;end
+class TestStark < Test::Unit::TestCase
+  def setup
+    @m = Module.new
+  end
 
   def test_materialize_parsing_errors
     file_path = File.join(File.dirname(__FILE__), 'parsing_error.thrift')
@@ -23,44 +25,43 @@ class TestSpark < Test::Unit::TestCase
   def test_materialize_service_with_struct_list
     file_path = File.join(File.dirname(__FILE__), 'properties.thrift')
     assert_nothing_raised do
-      Stark.materialize file_path, TestSparkScope
+      Stark.materialize file_path, @m
     end
-    assert TestSparkScope.const_defined?(:Property)
+    assert @m.const_defined?(:Property)
   end
 
   def test_materialize_service_with_struct_set
     file_path = File.join(File.dirname(__FILE__), 'users.thrift')
     assert_nothing_raised do
-      Stark.materialize file_path, TestSparkScope
+      Stark.materialize file_path, @m
     end
-    assert TestSparkScope.const_defined?(:User)
-    assert TestSparkScope.const_defined?(:FavoriteUsers)
+    assert @m.const_defined?(:User)
+    assert @m.const_defined?(:FavoriteUsers)
   end
 
   def test_materialize_service_with_comments
     file_path = File.join(File.dirname(__FILE__), 'comments.thrift')
     assert_nothing_raised do
-      Stark.materialize file_path, TestSparkScope
+      Stark.materialize file_path, @m
     end
-    assert TestSparkScope.const_defined?(:Foo)
+    assert @m.const_defined?(:Foo)
   end
 
   def test_materialize_service_with_throws
     file_path = File.join(File.dirname(__FILE__), 'types.thrift')
     assert_nothing_raised do
-      Stark.materialize file_path, TestSparkScope
+      Stark.materialize file_path, @m
     end
-    assert TestSparkScope.const_defined?(:Types)
+    assert @m.const_defined?(:Types)
   end
 
   def test_materialize_with_stringio
     file_path = File.join(File.dirname(__FILE__), 'types.thrift')
     io = StringIO.new(File.read(file_path))
-    m = Module.new
     assert_nothing_raised do
-      Stark.materialize io, m
+      Stark.materialize io, @m
     end
-    assert m.const_defined?(:Types)
+    assert @m.const_defined?(:Types)
   end
 
 end
