593df2c591e3297a57d2c3ce79736e94cb416009
Nick Sieger <nick@nicksieger.com>
2013-04-24 13:11:53 -0500

    Two-way conversion to or from strings (including container elements)

--- lib/stark/protocol_helpers.rb
+++ lib/stark/protocol_helpers.rb
@@ -1,30 +1,52 @@
 module Stark
   module ProtocolHelpers
+    COERCION_TO_STRING = Hash.new {|h,k|
+      lambda {|ip| ip.skip k; nil }
+    }.update({
+      ::Thrift::Types::BOOL   => lambda {|ip| ip.read_bool.to_s },
+      ::Thrift::Types::BYTE   => lambda {|ip| ip.read_byte.to_s },
+      ::Thrift::Types::I16    => lambda {|ip| ip.read_i16.to_s },
+      ::Thrift::Types::I32    => lambda {|ip| ip.read_i32.to_s },
+      ::Thrift::Types::I64    => lambda {|ip| ip.read_i64.to_s },
+      ::Thrift::Types::DOUBLE => lambda {|ip| ip.read_double.to_s },
+    })
+
+    COERCION_FROM_STRING = Hash.new {|h,k|
+      lambda {|val| nil }
+    }.update({
+      ::Thrift::Types::BOOL   => lambda {|val|
+        val =~ /^(1|y|t|on)/ ? true : false },
+      ::Thrift::Types::BYTE   => lambda {|val| val.to_i },
+      ::Thrift::Types::I16    => lambda {|val| val.to_i },
+      ::Thrift::Types::I32    => lambda {|val| val.to_i },
+      ::Thrift::Types::I64    => lambda {|val| val.to_i },
+      ::Thrift::Types::DOUBLE => lambda {|val| val.to_f },
+    })
+
     def expect(ip, expected, actual)
+      return nil if actual == ::Thrift::Types::STOP
+
       if expected == actual
         yield
+      elsif expected == ::Thrift::Types::STRING
+        COERCION_TO_STRING[actual][ip]
       elsif actual == ::Thrift::Types::STRING
-        value = ip.read_string
-        case expected
-        when ::Thrift::Types::BOOL
-          value =~ /^(1|y|t|on)/ ? true : false
-        when ::Thrift::Types::BYTE, ::Thrift::Types::I16,
-          ::Thrift::Types::I32, ::Thrift::Types::I64
-          value.to_i
-        when ::Thrift::Types::DOUBLE
-          value.to_f
-        else
-          nil
-        end
+        COERCION_FROM_STRING[expected][ip.read_string]
       else
-        ip.skip actual unless actual == ::Thrift::Types::STOP
+        ip.skip actual
         nil
       end
     end
 
+    def valid_element_type?(expected, actual)
+      expected == actual ||
+        expected == ::Thrift::Types::STRING ||
+        actual == ::Thrift::Types::STRING
+    end
+
     def expect_list(ip, expected)
       actual, size = ip.read_list_begin
-      if expected == actual
+      if valid_element_type?(expected, actual)
         yield actual, size
       else
         size.times { ip.skip actual }
@@ -36,7 +58,8 @@ module Stark
 
     def expect_map(ip, expected_key, expected_value)
       actual_key, actual_value, size = ip.read_map_begin
-      if expected_key == actual_key && expected_value == actual_value
+      if valid_element_type?(expected_key, actual_key) &&
+          valid_element_type?(expected_value, actual_value)
         yield actual_key, actual_value, size
       else
         size.times do
@@ -51,7 +74,7 @@ module Stark
 
     def expect_set(ip, expected)
       actual, size = ip.read_set_begin
-      if expected == actual
+      if valid_element_type?(expected, actual)
         yield actual, size
       else
         size.times { ip.skip actual }
--- test/test_coerce_strings.rb
+++ test/test_coerce_strings.rb
@@ -5,7 +5,7 @@ require 'thrift'
 
 require 'test/test_helper'
 
-class TestCoerceStrings < Test::Unit::TestCase
+class TestCoerceStrings1 < Test::Unit::TestCase
   IDL = "test/ThriftSpec.thrift"
   SERVICE = "SpecNamespace::NonblockingService"
   include TestHelper
@@ -45,5 +45,66 @@ class TestCoerceStrings < Test::Unit::TestCase
     end
 
     assert_equal 1.5, @handler.sleep_value
+  end
+end
+
+class TestCoerceStrings2 < Test::Unit::TestCase
+  IDL = "test/profile.thrift"
+  SERVICE = "UserStorage"
+  include TestHelper
+
+  def setup
+    setup_server
+  end
+
+  def test_coerce_string_to_int
+
+    send_to_server do
+      name = "add"
+      @client_p.write_message_begin name, Thrift::MessageTypes::CALL, 0
+      @client_p.write_struct_begin "#{name}_args"
+      @client_p.write_field_begin "a", Thrift::Types::STRING, 1
+      @client_p.write_string "1"
+      @client_p.write_field_end
+      @client_p.write_field_begin "b", Thrift::Types::STRING, 1
+      @client_p.write_string "1"
+      @client_p.write_field_end
+      @client_p.write_field_stop
+      @client_p.write_struct_end
+      @client_p.write_message_end
+      @client_p.trans.flush
+
+      @client_p.read_message_begin
+      @client_p.read_struct_begin
+      _, _, fid = @client_p.read_field_begin
+      assert_equal 0, fid
+      assert_equal 2, @client_p.read_i32
+      @client_p.read_field_end
+      _, type, _ = @client_p.read_field_begin
+      assert_equal type, Thrift::Types::STOP
+      @client_p.read_struct_end
+      @client_p.read_message_end
+    end
+  end
+
+  def test_coerce_number_list_to_string_list
+    send_to_server do
+      name = "set_list"
+      @client_p.write_message_begin name, Thrift::MessageTypes::CALL, 0
+      @client_p.write_struct_begin "#{name}_args"
+      @client_p.write_field_begin "l", Thrift::Types::LIST, 1
+      @client_p.write_list_begin ::Thrift::Types::I32, 3
+      @client_p.write_i32 1
+      @client_p.write_i32 2
+      @client_p.write_i32 3
+      @client_p.write_list_end
+      @client_p.write_field_end
+      @client_p.write_field_stop
+      @client_p.write_struct_end
+      @client_p.write_message_end
+      @client_p.trans.flush
+    end
+
+    assert_equal ["1", "2", "3"], @handler.last_list
   end
 end
